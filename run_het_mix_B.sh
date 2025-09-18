366f2016fd6c2efc288efd902406a5486ffb2902
Haolun Wu <573214193@qq.com>
2025-09-18 16:55:03 -0400

    update exp settings

--- run_het_mix_B.sh
+++ run_het_mix_B.sh
@@ -17,10 +17,16 @@ LLM_CONFIGS='[
 ]'
 
 # Run the debate with flexible configuration using key=value format
-python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=4 game_id_range=1,100 self_reported_confidence=false
-python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=4 game_id_range=1,100 self_reported_confidence=true
-python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=6 game_id_range=1,100 self_reported_confidence=false
-python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=6 game_id_range=1,100 self_reported_confidence=true
-python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=9 game_id_range=1,100 self_reported_confidence=false
-python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=9 game_id_range=1,100 self_reported_confidence=true
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=4 game_id_range=1,50 self_reported_confidence=false
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=4 game_id_range=1,50 self_reported_confidence=true
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=5 game_id_range=1,50 self_reported_confidence=false
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=5 game_id_range=1,50 self_reported_confidence=true
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=6 game_id_range=1,50 self_reported_confidence=false
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=6 game_id_range=1,50 self_reported_confidence=true
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=7 game_id_range=1,50 self_reported_confidence=false
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=7 game_id_range=1,50 self_reported_confidence=true
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=8 game_id_range=1,50 self_reported_confidence=false
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=8 game_id_range=1,50 self_reported_confidence=true
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=9 game_id_range=1,50 self_reported_confidence=false
+python run_debate.py flexible llm_configs="$LLM_CONFIGS" game_size=9 game_id_range=1,50 self_reported_confidence=true
 
